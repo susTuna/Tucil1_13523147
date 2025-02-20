c86294aa869dcbecbb65d653f9fa8c7ee3ee6e6c
susTuna <hidasetsuna@gmail.com>
2025-02-20 15:07:09 +0700

    feat: added Board Object

---
+++ src/Board.java
@@ -0,0 +1,32 @@
+public class Board {
+    private int height,width;
+    private char[][] grid;
+
+    public void genBoard(int a, int b){
+        this.height = a;
+        this.width = b;
+        this.grid = new char[height][width];
+
+        for (int i = 0; i < height; i++){
+            for (int j = 0; j < width; j++){
+                this.grid[i][j] = '0';
+            }
+        }
+    }
+
+    public void printBoard(){
+        for (int i = 0; i < height; i++){
+            for (int j = 0; j < width; j++){
+                System.out.print(grid[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Board board = new Board();
+        board.genBoard(5, 5);
+        board.printBoard();
+    }
+
+}
