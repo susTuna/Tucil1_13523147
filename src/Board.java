cff957d5ce6847862e13177f9b68f1eac91d1f80
susTuna <hidasetsuna@gmail.com>
2025-02-20 16:13:16 +0700

    feat: added color coding for different board pieces

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,5 @@
 public class Board {
+    public static final String RESET = "\u001B[37m";  // reset to default (white)
     private int height,width;
     private char[][] grid;
 
@@ -9,7 +10,7 @@ public class Board {
 
         for (int i = 0; i < height; i++){
             for (int j = 0; j < width; j++){
-                this.grid[i][j] = '0';
+                this.grid[i][j] = 'X';
             }
         }
     }
@@ -25,16 +26,50 @@ public class Board {
     public char[][] getGrid(){
         return this.grid;
     }
-    
-    public void printBoard(){
-        for (int i = 0; i < height; i++){
-            for (int j = 0; j < width; j++){
-                System.out.print(grid[i][j]);
-            }
-            System.out.println();
+
+    public void printBoard() {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            char piece = grid[i][j];
+            String color = getColor(piece); 
+            System.out.print(color + piece + RESET + "");
+        }
+        System.out.println();
         }
     }
 
+    private String getColor(char piece) {
+        switch (piece) {
+            case 'A': return "\u001B[31m"; // Red
+            case 'B': return "\u001B[32m"; // Green
+            case 'C': return "\u001B[34m"; // Blue
+            case 'D': return "\u001B[33m"; // Yellow
+            case 'E': return "\u001B[35m"; // Purple
+            case 'F': return "\u001B[36m"; // Cyan
+            case 'G': return "\u001B[91m"; // Bright Red
+            case 'H': return "\u001B[92m"; // Bright Green
+            case 'I': return "\u001B[93m"; // Bright Yellow
+            case 'J': return "\u001B[94m"; // Bright Blue
+            case 'K': return "\u001B[95m"; // Bright Magenta
+            case 'L': return "\u001B[96m"; // Bright Cyan
+            case 'M': return "\u001B[97m"; // Bright White
+            case 'N': return "\u001B[90m"; // Bright Black (Gray)
+            case 'O': return "\u001B[41m\u001B[30m"; // Red Background, Black Text
+            case 'P': return "\u001B[42m\u001B[30m"; // Green Background, Black Text
+            case 'Q': return "\u001B[44m\u001B[30m"; // Blue Background, Black Text
+            case 'R': return "\u001B[45m\u001B[30m"; // Magenta Background, Black Text
+            case 'S': return "\u001B[46m\u001B[30m"; // Cyan Background, Black Text
+            case 'T': return "\u001B[41m\u001B[37m"; // Red Background, White Text
+            case 'U': return "\u001B[42m\u001B[37m"; // Green Background, White Text
+            case 'V': return "\u001B[43m\u001B[37m"; // Yellow Background, White Text
+            case 'W': return "\u001B[44m\u001B[37m"; // Blue Background, White Text
+            case 'X': return "\u001B[45m\u001B[37m"; // Magenta Background, White Text
+            case 'Y': return "\u001B[46m\u001B[37m"; // Cyan Background, White Text
+            case 'Z': return "\u001B[47m\u001B[30m"; // White Background, Black Text
+            default:  return "\u001B[37m"; // White
+        }
+    }
+    
     public static void main(String[] args) {
         Board board = new Board();
         board.genBoard(5, 5);
